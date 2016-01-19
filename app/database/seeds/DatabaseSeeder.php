0a6c9831783d3fa7acc3c877122429a6df62b1ac
Anthony Burns <aburns42@gmail.com>
2016-01-19 14:52:19 -0600

    New files for the studyquestions table and to seed said table.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,11 +11,15 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
+		DB::table('studyquestions')->delete();
+		DB::table('notes')->delete();
 		DB::table('studylists')->delete();
 		DB::table('users')->delete();
 
 		$this->call('UsersSeeder');
 		$this->call('StudylistTableSeeder');
+		$this->call('NotesTableSeeder');
+		$this->call('StudyquestionsTableSeeder');
 	}
 
 }
