58ca2211db00ee59c98efe59a7474b2809264aee
Anthony Burns <aburns42@gmail.com>
2016-01-20 22:53:18 -0600

    Updated files, sheets create form now saving all data to db. jQuery makes new lines in form on click and SheetsController store function saves to both tables (sheets and lines).

--- app/views/sheets/create.blade.php
+++ app/views/sheets/create.blade.php
@@ -1,1 +1,66 @@
-create.blade.php
+@extends('layouts.master')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-2 text-center">
+                <a href="{{{ action('SheetsController@index') }}}">Back to Sheets Index</a>
+            </div> <!-- end col-md-2 -->
+
+            <div class="col-md-8">
+                
+                <h2 class="text-center">Create a New Sheet</h2>
+                {{ Form::open(array('action' => 'SheetsController@store')) }}
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="form-group">
+                                {{ $errors->first('title', '<span class="help-block">:message</span>') }}    
+
+                                {{ Form::label('title', 'Title') }}
+                                {{ Form::text('title', null, ['class' => 'form-control']) }}
+                            </div>  <!-- end form-group -->
+                        </div> <!-- end col-xs-12 -->
+                    </div> <!-- end row -->
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <div class="form-group">
+                                {{ $errors->first('public_or_private', '<span class="help-block">:message</span>') }}    
+
+                                <label>Privacy Setting (Choose One): </label>
+                                <label class="radiomargin">
+                                    <input type="radio" name="public_or_private" id="public_or_private1" value="public"> Public
+                                </label>
+                                <label class="radiomargin">
+                                    <input type="radio" name="public_or_private" id="public_or_private2" value="private"> Private
+                                </label>
+                            </div> <!-- end form-group -->
+                        </div> <!-- end col-xs-6 -->
+                    </div> <!-- end row -->
+                    <div class="row lines">
+                        <div class="col-xs-6">
+                            <div class="form-group">
+                                {{ $errors->first('clue', '<span class="help-block">:message</span>') }}    
+
+                                {{ Form::label('clue', 'Clue') }}
+                                {{ Form::text('clue', null, ['class' => 'form-control']) }}
+                            </div> <!-- end form-group -->
+                        </div> <!-- end col-xs-6 -->
+                        <div class="col-xs-6">
+                            <div class="form-group">
+                                {{ $errors->first('response', '<span class="help-block">:message</span>') }}    
+
+                                {{ Form::label('response', 'Response') }}
+                                {{ Form::text('response', null, ['class' => 'form-control']) }}
+                            </div> <!-- end form-group -->
+                        </div> <!-- end col-xs-6 -->
+                    </div> <!-- end row -->
+                    <button id="makeline" class="btn btn-default formmargin" type="button">Make New Clue/Response</button>
+                    <button class="btn btn-default formmargin" type="submit">Submit Sheet</button>
+                {{ Form::close() }}
+
+            </div> <!-- end col-md-8 -->
+
+        </div> <!-- end row -->
+    </div> <!-- end container -->
+@stop
--- public/js/sheets.js
+++ public/js/sheets.js
@@ -1,12 +1,40 @@
 $(document).ready(function() { 
-	"Use Strict";
+    "Use Strict";
 
-	$('#infotitle').click(function() {
-	        $('.infobox').slideToggle();
-	    });
+    // Slides the info box into view.
+    $('#infotitle').click(function() {
+        $('.infobox').slideToggle();
+    });
 
-	$(".clueclick").click(function() {
+    // $(".clueclick").click(function() {
+    //     $(".responseslide").fadeToggle();
+    // });
+    // This opens all responses on the click of this btn.
+    $("#toggle-btn").click(function() { 
         $(".responseslide").fadeToggle();
+    });
+    // Next targets one dd at a time.
+    $("dt").click(function() {
+        $(this).next("dd").fadeToggle();
+    });
+
+    var i = 1;
+    $("#makeline").click(function() {
+        var linesHTML = "<div class='col-xs-6'>";
+            linesHTML += "<div class='form-group'>";
+            linesHTML += "<label for='clue" + i + "'>Clue" + i + "</label>";
+            linesHTML += "<input class='form-control' name='clue" + i + "' type='text' id='clue" + i + "'>";
+            linesHTML += "</div>";
+            linesHTML += "</div>";
+            linesHTML += "<div class='col-xs-6'>";
+            linesHTML += "<div class='form-group'>";
+            linesHTML += "<label for='response" + i + "'>Response" + i + "</label>";
+            linesHTML += "<input class='form-control' name='response" + i + "' type='text' id='response" + i + "'>";
+            linesHTML += "</div>";
+            linesHTML += "</div>";
+        $(".lines").append(linesHTML)
+        i = i + 1;
+        console.log(i);
     });
 
 });
