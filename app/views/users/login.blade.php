b0c44cb0097678f5a0e3987c52f4bfb9a0ad2b85
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-01-27 09:41:20 -0600

    got the csrf filters working properly on the user pages

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="col-lg-12 text-center">
 			<h3>Enter your information to log in!</h3>
-			<form class="form-horizontal text-center col-lg-4 col-lg-offset-4" method="POST" action="{{{action('UsersController@dologin')}}}">
+			{{Form::open(array('action' => 'UsersController@dologin'))}}
 				<div class="form-group">
 					<label class="control-label" for "email">Email</label>
 					<input type="text" class="form-control" name="email">
@@ -14,7 +14,7 @@
 					<input type="password" class="form-control" name="password">
 				</div>
 				<button class="btn btn-standard">Log In!</button>
-			</form>
+			{{Form::close()}}
 		</div>
 	</div>
 </div>
